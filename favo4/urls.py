4b179dc62d8bc761d43c85bd011bc2d4b6a35127
akifuji <akifujitonakai@icloud.com>
2021-03-11 23:52:20 +0900

    フォーム追加(inlineとformsetの複合) カスタムユーザー スーパーユーザー
    問題点:
    フォーム投稿時ユーザー選択しなければならない
    detailで取り出しすると全表示になってしまう
     pkなりで制御したいが…

---
+++ favo4/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+
+
+app_name = 'favo4'
+urlpatterns = [
+    path('', views.HomeView.as_view(), name="index"),
+    path('home/', views.F4ListView.as_view(), name="F4-list"),
+    path('F4/<int:pk>-detail/', views.F4DetailView.as_view(), name="F4-detail"),
+    path('new/', views.add_content, name="F4-create"),
+    path('home/<int:pk>/', views.F4DetailView.as_view(), name="F4-detail"),
+]
