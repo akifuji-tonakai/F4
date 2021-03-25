4abd1568cb28c6fba3aa82983ed741205e437afe
akifuji <akifujitonakai@icloud.com>
2021-03-25 13:40:16 +0900

    oauth不可能バグ進行中

--- F4/urls.py
+++ F4/urls.py
@@ -17,13 +17,13 @@ from django.contrib import admin
 from django.contrib.staticfiles.urls import static
 from django.urls import path, include
 
-# from . import settings_common, settings_dev
+from . import settings_common, settings_dev
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('favo4.urls')),
     path('accounts/any/', include('allauth.urls')),
-    path('', include('social_django.urls')),  # 追加
+    path(r'', include('social_django.urls', namespace='social')),
 ]
 
-# urlpatterns += static(settings_common.MEDIA_URL, document_root=settings_dev.MEDIA_ROOT)
+urlpatterns += static(settings_common.MEDIA_URL, document_root=settings_dev.MEDIA_ROOT)
