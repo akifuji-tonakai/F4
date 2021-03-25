f4368ea7148bd4f29fa0fb34f26b7bd6bade9d8b
akifuji <akifujitonakai@icloud.com>
2021-03-25 15:51:53 +0900

    セキュリティ対策

--- F4/settings.py
+++ F4/settings.py
@@ -17,79 +17,65 @@ MEDIA_ROOT = '/usr/share/nginx/html/media'
 # EMAIL_BACKEND = 'django_ses.SESBackend'
 
 # ロギング
-# LOGGING = {
-#     'version': 1,
-#     'disable_existing_loggers': False,
-#
-#     # ロガーの設定
-#     'loggers': {
-#         # Djangoが利用するロガー
-#         'django': {
-#             'handlers': ['file'],
-#             'level': 'INFO',
-#         },
-#         # diaryアプリケーションが利用するロガー
-#         'diary': {
-#             'handlers': ['file'],
-#             'level': 'INFO',
-#         },
-#     },
-#
-#     # ハンドラの設定
-#     'handlers': {
-#         'file': {
-#             'level': 'INFO',
-#             'class': 'logging.handlers.TimedRotatingFileHandler',
-#             'filename': os.path.join(BASE_DIR, 'logs/django.log'),
-#             'formatter': 'prod',
-#             'when': 'D',  # ログローテーション(新しいファイルへの切り替え)間隔の単位(D=日)
-#             'interval': 1,  # ログローテーション間隔(1日単位)
-#             'backupCount': 7,  # 保存しておくログファイル数
-#         },
-#     },
-#
-#     # フォーマッタの設定
-#     'formatters': {
-#         'prod': {
-#             'format': '\t'.join([
-#                 '%(asctime)s',
-#                 '[%(levelname)s]',
-#                 '%(pathname)s(Line:%(lineno)d)',
-#                 '%(message)s'
-#             ])
-#         },
-#     }
-# }
-
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
-    'formatters': {
-        'production': {
-            'format': '%(asctime)s [%(levelname)s] %(process)d %(thread)d '
-                      '%(pathname)s:%(lineno)d %(message)s'
-        },
-    },
-    'handlers': {
-        'file': {
-            'class': 'logging.FileHandler',
-            'filename': 'logs/django.log',  #環境に合わせて変更
-            'formatter': 'production',
-            'level': 'INFO',
-        },
-    },
+
+    # ロガーの設定
     'loggers': {
-        # 自作したログ出力
-        '': {
-            'handlers': ['file'],
-            'level': 'INFO',
-            'propagate': False,
-        },
-        # Djangoの警告・エラー
+        # Djangoが利用するロガー
         'django': {
             'handlers': ['file'],
             'level': 'INFO',
-            'propagate': False,
+        },
+        # diaryアプリケーションが利用するロガー
+        'diary': {
+            'handlers': ['file'],
+            'level': 'INFO',
         },
     },
+
+    # ハンドラの設定
+    'handlers': {
+        'file': {
+            'level': 'INFO',
+            'class': 'logging.handlers.TimedRotatingFileHandler',
+            'filename': os.path.join(BASE_DIR, 'logs/django.log'),
+            'formatter': 'prod',
+            'when': 'D',  # ログローテーション(新しいファイルへの切り替え)間隔の単位(D=日)
+            'interval': 1,  # ログローテーション間隔(1日単位)
+            'backupCount': 7,  # 保存しておくログファイル数
+        },
+    },
+
+    # フォーマッタの設定
+    'formatters': {
+        'prod': {
+            'format': '\t'.join([
+                '%(asctime)s',
+                '[%(levelname)s]',
+                '%(pathname)s(Line:%(lineno)d)',
+                '%(message)s'
+            ])
+        },
+    }
 }
+
+# セキュリティ関連設定
+# security.W004
+SECURE_HSTS_SECONDS = 31536000
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+# security.W006
+# SECURE_CONTENT_TYPE_NOSNIFF = True
+# security.W007
+# SECURE_BROWSER_XSS_FILTER = True
+# security.W008
+SECURE_SSL_REDIRECT = True
+# security.W012
+SESSION_COOKIE_SECURE = True
+# security.W016
+CSRF_COOKIE_SECURE = True
+# security.W019
+# X_FRAME_OPTIONS = 'DENY'
+# security.W021
+# SECURE_HSTS_PRELOAD = True
